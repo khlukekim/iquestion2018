09a556fbccfac33fbfadc09aff95bfed91c475eb
doiluvu <doiluvu@gmail.com>
2018-10-05 02:31:56 +0900

    upload image

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 from pymongo import MongoClient
+from werkzeug.utils import secure_filename 
+import datetime, os
+
 app = Flask(__name__)
+
+USER_IMAGE_FOLDER = os.path.join('static', 'userimage')
+app.config['USER_IMAGE_FOLDER'] = USER_IMAGE_FOLDER
+
+database_information = ['127.0.0.1', '27017']
+with open('database') as f:
+  database_information = [x.strip() for x in f.readlines()]
+
 
 def option():
   return {
@@ -34,17 +45,39 @@ def exhibit(size, col, row, margin):
     })
 
 
-@app.route('/image', method='POST')
+@app.route('/image', methods=['POST'])
 def upload_image():
-  # todo
-  # save image
-  # process
-  # insert
-  #   time
-  #   point
-  #  filename
-  #  n id
-  #  hash id
+  if 'user_image' not in request.files:
+    # no file
+    # what should I do?
+    # maybe just redirect back
+    # or use json error response
+    return jsonify({
+      'r': 'f',
+      'c': 'nofile'
+      })
+  file = request.files['user_image']
+  if file.filename == '':
+    return jsonify({
+      'r': 'f',
+      'c': 'nofile'
+      })
+  if file:
+    with MongoDBConnection(database_information[0], database_information[1]) as mongo:
+      coll = mongo.connection.iquestion.userImages 
+      data = {
+        'created_at': datetime.datetime.now(),
+        'prediction_point': 0,
+        'original_filename': file.filename,
+      } 
+      db_result = coll.insert_one(data)
+
+      filename = str(db_result.inserted_id) + '.' + file.filename.split('.')[-1]
+      file.save(os.path.join(app.config['USER_IMAGE_FOLDER'], filename))
+      
+      return jsonify({
+        'r': 's'
+        })
 
 @app.route('/perf/<int:width>/<int:height>')
 def perform(width, height):
@@ -67,7 +100,7 @@ class MongoDBConnection(object):
 
   def __init__(self, host='localhost', port='27017'):
     self.host = host
-    self.port = port
+    self.port = int(port)
     self.connection = None
 
   def __enter__(self):
