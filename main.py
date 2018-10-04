292a3b1e4bf616ea338971da39e32a7438c431a0
doiluvu <doiluvu@gmail.com>
2018-10-04 18:22:19 +0900

    park page

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 @app.route('/')
-def root():
+def index():
+  return render_template('park.html')
+
+@app.route('/test')
+def user():
   return render_template('main_plain.html', option={
     'sketchjs':url_for('static', filename='sketch.js'),
     'nanumfont':url_for('static', filename='NanumBarunGothicBold.otf'),
