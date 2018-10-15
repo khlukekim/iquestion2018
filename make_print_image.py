01842779e2755f37d91023bd46a17a19b27786b7
khlk <khlukekim@gmail.com>
2018-10-15 06:05:12 +0000

    modified print font

--- make_print_image.py
+++ make_print_image.py
@@ -1,24 +1,28 @@
 from PIL import Image, ImageDraw, ImageFont
+import datetime
 
-im = Image.new('RGB', (4040, 600))
+im = Image.new('RGB', (4140, 600))
 dr = ImageDraw.Draw(im)
 for i in range(0, 64):
     for j in range(0, 10):
         p = Image.open('static/images/size_56/%04d.jpg'%(j * 64 + i + 1))
-        im.paste(p,(100 + i * 60, j * 60)) 
+        im.paste(p,(150 + i * 60, j * 60)) 
 
-f = ImageFont.truetype('secrcode.ttf', 90)
+f1 = ImageFont.truetype('jackinput.TTF', 90)
 tx = Image.new('RGB', (600, 100))
 dr = ImageDraw.Draw(tx)
-dr.text((60, 0),'I Question', font=f)
+dr.text((60, 0),'I Question', font=f1)
 r = tx.rotate(90, expand=1)
-im.paste(r, (0, 0))
+im.paste(r, (50, 0))
 
+f2 = ImageFont.truetype('jackinput.TTF', 84)
 tx2 = Image.new('RGB', (600, 100))
 dr2 = ImageDraw.Draw(tx2)
-dr2.text((20, 0), '201810122101', font=f)
+d = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
+timestring = "%d%02d%02d%02d%02d"%(d.year, d.month, d.day, d.hour, d.minute)
+dr2.text((0, 0), timestring, font=f2)
 rr = tx2.rotate(90, expand=1)
-im.paste(rr, (3940, 0))
+im.paste(rr, (3990, 0))
 s = im.rotate(90, expand=1)
 im.save('static/printtest.jpg')
 s.save('static/printtest2.jpg')
